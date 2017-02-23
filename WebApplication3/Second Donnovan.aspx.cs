5b660edd2ba163a641d42d35ed37194647b061d7
omdy <i.omdymc@gmail.com>
2017-02-23 16:12:36 -0500

    Adding Methods

--- WebApplication3/Second Donnovan.aspx.cs
+++ WebApplication3/Second Donnovan.aspx.cs
@@ -30,8 +30,21 @@ namespace WebApplication3
 
             MySqlConnection con = new MySqlConnection("server=localhost;user id=root;persistsecurityinfo=True;database=chartofaccounts;password=andy");
             con.Open();
-            String coaDB = "INSERT INTO chartofaccounts.chartofaccounts VALUES('" + TextBox2.Text + "','" + DropDownList1.Text + "','" + DropDownList2.Text + "','" + TextBox6.Text + "','" + TextBox7.Text + "','" + user + "','" + date + "','" + DropDownList3.Text + "','" + TextBox11.Text + "')";
-            MySqlCommand cmd = new MySqlCommand(coaDB, con);
+
+            string accName = TextBox2.Text;
+            string accType = DropDownList1.Text;
+            string accSide = DropDownList2.Text;
+            string accCode = TextBox6.Text;
+            string accOrder = TextBox7.Text;
+            string accUser = user;
+            string accDate = date;
+            string accActive = DropDownList3.Text;
+            string accComment = TextBox11.Text;
+
+            bool insertWork;
+            insertAcc(user, date, accName, accOrder, accCode, accType, accSide, accSide, accComment);
+
+
 
             String dbType = "Account";
             String dbEvent = "Added " + TextBox2.Text;
@@ -43,7 +56,7 @@ namespace WebApplication3
 
             try
             {
-                cmd.ExecuteNonQuery();
+                
                 
                 cmd2.ExecuteNonQuery();
 
@@ -61,7 +74,7 @@ namespace WebApplication3
 
 
             cmd2.Dispose();
-            cmd.Dispose();
+            
             con.Close();
             Response.Write("Submitted to database. Good job!");
         }
@@ -69,6 +82,28 @@ namespace WebApplication3
         protected void Button3_Click(object sender, EventArgs e)
         {
             Response.Redirect("~/Default.aspx");
+        }
+        public Boolean insertAcc(string user, string date, string accName, string order, string code, string type, string side, string active, string comment)
+        {
+            MySqlConnection conIA = new MySqlConnection("server=localhost;user id=root;persistsecurityinfo=True;database=chartofaccounts;password=andy");
+            conIA.Open();
+
+            String coaDB = "INSERT INTO chartofaccounts.chartofaccounts VALUES('" + accName + "','" + type + "','" + side + "','" + code + "','" + order + "','" + user + "','" + date + "','" + active + "','" + comment + "')";
+            MySqlCommand cmdIA = new MySqlCommand(coaDB, conIA);
+
+            try
+            {
+                cmdIA.ExecuteNonQuery();                
+            }
+            catch
+            {
+                Response.Write("Your account was not submitted. Please review the information submitted.");
+                return false;
+            }
+            
+            cmdIA.Dispose();
+            conIA.Close();
+            return true;
         }
     }
 }
--- WebApplication3/Testing.aspx.cs
+++ WebApplication3/Testing.aspx.cs
@@ -36,28 +36,80 @@ namespace WebApplication3
 
         protected void Button2_Click(object sender, EventArgs e)
         {
-            /*
-            MySqlConnection con = new MySqlConnection("server=localhost;user id=root;persistsecurityinfo=True;database=chartofaccounts;password=andy");
-            con.Open();
+            string tbl = "accountbalances";
+            string acc = "Cash";
+            string place = "Actual Balance";
+            string s = "";
 
-            MySqlCommand cmd = new MySqlCommand("select * from chartofaccounts.accountbalances ORDER BY `Liquidity Order`", con);
-            MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
-            DataSet ds = new DataSet();
-            adp.Fill(ds);
-
-            GridView2.DataSource = ds;
-            GridView2.DataBind();
-
-            cmd.Dispose();
-            con.Close();*/
-
-
-
-    
-
-
-
-            
+            double addThem = getValueDB(tbl, acc, place) + getValueDB(tbl, acc, place);
+            s = "" + addThem;
+            TextBox1.Text = s;
+                
         }
+
+        public double getValueDB(string tbl, string acc, string place)
+        {
+            double dbVal = 0;
+            MySqlConnection conVal = new MySqlConnection("server=localhost;user id=root;persistsecurityinfo=True;database=chartofaccounts;password=andy");
+            conVal.Open();
+            String sVal = "SELECT `" + place + "` FROM `chartofaccounts`.`" + tbl + "` WHERE `Account` =  '" + acc + "'";
+            MySqlCommand cmdVal = new MySqlCommand(sVal, conVal);
+            try
+            {
+                MySqlDataReader reader2 = cmdVal.ExecuteReader();
+                if (reader2.Read())
+                {
+                    String s = reader2.GetValue(0).ToString();
+                    dbVal = Double.Parse(s);
+
+                }
+            }
+            catch
+            {
+                Response.Write(sVal);
+            }
+            
+
+            cmdVal.Dispose();
+            conVal.Close();
+            return dbVal;
+        }
+
+        public void insertAcc(string user, string date, string accName, string order, string code, string type, string side, string active, string comment)
+        {
+            MySqlConnection conIA = new MySqlConnection("server=localhost;user id=root;persistsecurityinfo=True;database=chartofaccounts;password=andy");
+            conIA.Open();
+            
+
+
+            String coaDB = "INSERT INTO chartofaccounts.chartofaccounts VALUES('" + accName + "','" + type + "','" + side + "','" + code + "','" + order + "','" + user + "','" + date + "','" + active + "','" + comment + "')";
+            MySqlCommand cmdIA = new MySqlCommand(coaDB, conIA);
+
+            try
+            {
+                cmdIA.ExecuteNonQuery();
+                
+            }
+            catch
+            {
+                Response.Write("Your account was not submitted. Please review the information submitted.");
+            }
+            cmdIA.Dispose();
+            conIA.Close();
+        }
+        
+
+        //UPDATE `chartofaccounts`.`journaltran` SET `Account1`= '" + TextBox7.Text + "', `Value1`= '" + TextBox5.Text + "', `Account2`= '" + TextBox8.Text + "', `Value2`= '" + TextBox6.Text + "' WHERE `ID`= '1';
+
+
+
+
+
+
+
+
+
+
+
     }
 }
